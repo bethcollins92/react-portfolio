a4c74dec8554cfd8de194bde685e7729666f5f05
Beth Collins <bethcollinsliv92@gmail.com>
2020-04-27 17:04:55 +0200

    getting started

---
+++ react-portfolio/src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div>
+      <h1>Hi I'm Beth, a Full Stack Developer</h1>
+    </div>
+  );
+}
+
+export default App;
