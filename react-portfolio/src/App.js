8ce3b3e94b192e3029708c785bc1b0e66df5334d
Beth Collins <bethcollinsliv92@gmail.com>
2020-04-29 14:35:09 +0200

    working on profile page

--- react-portfolio/src/App.js
+++ react-portfolio/src/App.js
@@ -2,12 +2,25 @@ import React from "react";
 import "./App.css";
 import Profile from "./components/Profile";
 import Blog from "./components/Blog";
+import Home from "./components/Home";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
 function App() {
   return (
-    <div>
-      <Profile />
-      <Blog />
+    <div className="App">
+      <Router>
+        <Switch>
+          <Route path="/profile">
+            <Profile />
+          </Route>
+          <Route path="/blog">
+            <Blog />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
--- react-portfolio/src/components/Profile.js
+++ react-portfolio/src/components/Profile.js
@@ -1,9 +1,26 @@
 import React from "react";
+import "./styling/Profile.css";
 
 function displayProfile() {
   return (
-    <div>
-      <h1>This is the profile page</h1>
+    <div className="container">
+      <div className="row">
+        <div id="col1" className="col-4">
+          <div>
+            <img
+              className="circular--square"
+              src="https://i.imgur.com/Y0cbrLW.jpg"
+              alt="portrait"
+            ></img>
+          </div>
+          <br></br>
+          <p>About me</p>
+          <p>Resume</p>
+          <p>Projects</p>
+          <p>Blog</p>
+        </div>
+        <div className="col-8"></div>
+      </div>
     </div>
   );
 }
